564c8328f1708698158fdb95629e899af667a878
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2024-02-28 20:32:57 +0100

    f

--- src/finml_utils/parallel.py
+++ src/finml_utils/parallel.py
@@ -6,7 +6,13 @@ from pathos.multiprocessing import Pool, cpu_count
 T = TypeVar("T")
 R = TypeVar("R")
 
-def pmap(func: Callable[[T], R], iterable: Sequence[T], n_jobs: int = -1, pool: Pool | None = None) -> list[R]:
+
+def pmap(
+    func: Callable[[T], R],
+    iterable: Sequence[T],
+    n_jobs: int = -1,
+    pool: Pool | None = None,
+) -> list[R]:
     assert n_jobs != 0
     n_cpus = cpu_count() if n_jobs == -1 else n_jobs
     pool = pool or Pool(n_cpus)
@@ -16,6 +22,6 @@ def pmap(func: Callable[[T], R], iterable: Sequence[T], n_jobs: int = -1, pool: 
             iterable,
             chunksize=max(1, int(len(iterable) // n_cpus)),
         )
-        if len(iterable) > 2 and n_cpus > 1
+        if len(iterable) > 2 and n_cpus > 1  # noqa: PLR2004
         else [func(el) for el in iterable]
     )
