712adaa1aea5ac87e5e70c39adae0495412b6d50
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2024-05-14 11:45:33 +0200

    fix(Returns): to_returns() should still accept a DataFrame

--- tests/test_returns.py
+++ tests/test_returns.py
@@ -1,11 +1,22 @@
 import numpy as np
 import pandas as pd
+from finml_utils.dataframes import concat_on_columns
 from finml_utils.returns import to_log_returns, to_returns
 
 
 def test_returns_clip():
     data = pd.Series([0.0, 0.0, 1.3, 1.4, 3.4, 1.5, 1.6, 1.7, 1.8, 1.9])
     assert not np.isinf(to_returns(data, clip=1.0)).any()
+
+
+def test_returns_dataframe():
+    data = pd.Series([0.0, 0.0, 1.3, 1.4, 3.4, 1.5, 1.6, 1.7, 1.8, 1.9], name="A")
+
+    assert (
+        not np.isinf(to_returns(concat_on_columns([data, data.rename("B")]), clip=1.0))
+        .any()
+        .any()
+    )
 
 
 def test_log_returns_clip():
