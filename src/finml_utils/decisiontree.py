0cf6507af51e5c530c404bcb565414524c4e67bc
quant@mppecapital.com <168167163+MPPE-Capital@users.noreply.github.com>
2024-10-07 11:31:16 +0200

    Rename TwoDimPiecewiseLinReg to TwoDimPiecewiseDecisionTree and extend logic of UltraRegularizedDecisionTree to two dimensions

--- src/finml_utils/decisiontree.py
+++ src/finml_utils/decisiontree.py
@@ -1,4 +1,5 @@
-from typing import Literal
+import math
+from typing import Literal, Tuple
 
 import numpy as np
 import pandas as pd
@@ -309,88 +310,181 @@ class UltraRegularizedDecisionTree(BaseEstimator, ClassifierMixin, MultiOutputMi
         return output
 
 
-class TwoDimPiecewiseLinReg(BaseEstimator, ClassifierMixin, MultiOutputMixin):
+class TwoDimPiecewiseDecisionTree(BaseEstimator, ClassifierMixin, MultiOutputMixin):
     def __init__(
         self,
-        threshold_margin: float,  # used to produce the range of deciles/percentiles when the model can split, 0.1 means the range is 0.4 to 0.6 percentile.
-        threshold_step: float,  # used to produce the range of deciles/percentiles when the model can split, 0.05 means the possible splits will be spaced 5% apart
-        positive_class: int,  # this model can not flip the "coefficient", so the positive class is fixed
-        num_splits: int = 4,  # number of extra splits to make around the best split, eg. if 2 and the best quantile is 0.5, then the splits will be [0.45, 0.5, 0.55]
+        threshold_margins: Tuple[float, float],  # used to produce the range of deciles/percentiles when the model can split, 0.1 means the range is 0.4 to 0.6 percentile.
+        threshold_steps: Tuple[float, float],  # used to produce the range of deciles/percentiles when the model can split, 0.05 means the possible splits will be spaced 5% apart
+        positive_classes: Tuple[int, int],  # this model can not flip the "coefficient", so the positive class is fixed
+        num_splitss: Tuple[int, int] = (4, 4),  # number of extra splits to make around the best split, eg. if 2 and the best quantile is 0.5, then the splits will be [0.45, 0.5, 0.55]
         aggregate_func: Literal["mean", "sharpe"] = "sharpe",
     ):
         self.aggregate_func = aggregate_func
-        assert threshold_margin <= 0.3, f"Margin too large: {threshold_margin}"
-        assert threshold_step <= 0.05, f"Step too large: {threshold_margin}"
-        self.num_splits = num_splits
-        self._positive_class = positive_class
-        if threshold_margin > 0:
-            threshold_margin = 0.5 - threshold_margin
+        assert len(threshold_margins) == 2, f"{len(threshold_margins)=} instead of 2"
+        assert len(threshold_steps) == 2, f"{len(threshold_steps)=} instead of 2"
+        assert len(positive_classes) == 2, f"{len(positive_classes)=} instead of 2"
+        assert len(num_splitss) == 2, f"{len(num_splitss)=} instead of 2"
+        assert all(
+            threshold_margin <= 0.3 for threshold_margin in threshold_margins
+        ), f"Margin(s) too large: {threshold_margins}"
+        assert all(
+            threshold_step <= 0.05 for threshold_step in threshold_steps
+        ), f"Step(s) too large: {threshold_margins}"
+        self.num_splitss = num_splitss
+        self._positive_classes = positive_classes
 
-            self.threshold_to_test = (
-                np.arange(
-                    threshold_margin, 1 - threshold_margin + 0.0001, threshold_step
+        self.thresholds_to_test = []
+        for threshold_margin, threshold_step in zip(threshold_margins, threshold_steps):
+            if threshold_margin > 0:
+                threshold_margin = 0.5 - threshold_margin
+
+                self.thresholds_to_test.append(
+                    np.arange(
+                        threshold_margin, 1 - threshold_margin + 0.0001, threshold_step
+                    )
+                    .round(3)
+                    .tolist()
                 )
-                .round(3)
-                .tolist()
-            )
-        else:
-            self.threshold_to_test = [0.5]
+            else:
+                self.thresholds_to_test.append([0.5])
+        self.thresholds_to_test = tuple(self.thresholds_to_test)
+
+        self._splits_0 = None
+        self._splits_1 = None
 
     def fit(
         self, X: pd.DataFrame, y: pd.Series, sample_weight: pd.Series | None = None
     ):
-        assert X.shape[1] == 1, "Only single feature supported"
+        assert X.shape[1] == 2, "Exactly two features are supported"
         X = X.squeeze()
-        splits = np.quantile(
-            X, self.threshold_to_test, axis=0, method="closest_observation"
-        )
-        if isinstance(X, pd.Series):
-            X = X.to_numpy()
+        splitss = [
+            np.quantile(
+                X[col], threshold_to_test, axis=0, method="closest_observation"
+            )
+            for col, threshold_to_test
+            in zip(X.columns, self.thresholds_to_test)
+        ]
         if isinstance(y, pd.Series):
             y = y.to_numpy()
-        differences = [
-            calculate_bin_diff(t, X=X, y=y, agg_method=self.aggregate_func)
-            for t in splits
-        ]
-        idx_best_split = np.argmax(np.abs(differences))
-        best_split = float(splits[idx_best_split])
-        if np.isnan(best_split):
-            self._splits = [splits[1]]
+
+        idx_best_split_0 = None
+        idx_best_split_1 = None
+        highest_abs_difference = None
+        # It could be that the best split comes from considering only the second column in X, not both.
+        for idx_1, split_1 in enumerate(splitss[1]):
+            difference_1 = calculate_bin_diff(
+                quantile=split_1, X=X[X.columns[1]], y=y, agg_method=self.aggregate_func
+            )
+            if highest_abs_difference is None or abs(difference_1) > highest_abs_difference:
+                highest_abs_difference = abs(difference_1)
+                idx_best_split_0 = None
+                idx_best_split_1 = idx_1
+
+        for idx_0, split_0 in enumerate(splitss[0]):
+            # It could be that the best split comes from considering only the first column in X, not both.
+            difference_0 = calculate_bin_diff(
+                quantile=split_0, X=X[X.columns[0]], y=y, agg_method=self.aggregate_func
+            )
+            if highest_abs_difference is None or abs(difference_0) > highest_abs_difference:
+                highest_abs_difference = abs(difference_0)
+                idx_best_split_0 = idx_0
+                idx_best_split_1 = None
+
+            # It could be that the best split comes from considering both columns in X.
+            for idx_1, split_1 in enumerate(splitss[1]):
+                difference = calculate_bin_diff(
+                    quantile_0=split_0, quantile_1=split_1, X=X, y=y, agg_method=self.aggregate_func
+                )
+                if highest_abs_difference is None or abs(difference) > highest_abs_difference:
+                    highest_abs_difference = abs(difference)
+                    idx_best_split_0 = idx_0
+                    idx_best_split_1 = idx_1
+
+        if idx_best_split_0 is None and idx_best_split_1 is None:
+            self._splits_0 = [splitss[0][1]]
+            self._splits_1 = [splitss[1][1]]
             return
 
-        best_quantile = self.threshold_to_test[idx_best_split]
-        deciles_to_split = (
-            list(
-                reversed(
-                    [
-                        best_quantile - (i * 0.01)
-                        for i in range(0, 6 * self.num_splits, 5)
-                    ][1:]
-                )
-            )
-            + [best_quantile]
-            + [best_quantile + (i * 0.01) for i in range(0, 6 * self.num_splits, 5)][1:]
-        )  # number of extra splits to make around the best split, eg. if 2 and the best quantile is 0.5, then the splits will be [0.45, 0.5, 0.55]
-        self._splits = np.quantile(
-            X,
-            [round(i, 2) for i in deciles_to_split],
-            axis=0,
-            method="nearest",
-        )  # translate the percentiles into actual values
-        assert np.isnan(self._splits).sum() == 0
+        pos_times = 6
 
-    def predict(self, X: pd.DataFrame) -> pd.Series:
-        assert self._positive_class is not None, "Model not fitted"
-        assert self._splits is not None, "Model not fitted"
+        deciles_to_split_0 = None
+        if idx_best_split_0 is not None:
+            best_quantile_0 = self.thresholds_to_test[0][idx_best_split_0]
+            deciles_to_split_0 = calc_deciles_to_split(best_quantile_0, self.num_splitss[0])
 
-        output = (
-            np.searchsorted(self._splits, X.squeeze(), side="right") / len(self._splits)
-        )  # find the value in the splits, the index of the split acts as a scaled value between 0 and 1
-        if isinstance(X, pd.DataFrame):
-            output = pd.Series(output, index=X.index)
-        if self._positive_class == 0:
-            output = 1 - output
-        return output
+        deciles_to_split_1 = None
+        if idx_best_split_1 is not None:
+            best_quantile_1 = self.thresholds_to_test[1][idx_best_split_1]
+            deciles_to_split_1 = calc_deciles_to_split(best_quantile_1, self.num_splitss[1])
+
+        # In case best split is created using values from columns,
+        # then reduce the number of deciles around the best split.
+        if idx_best_split_0 is not None and idx_best_split_1 is not None:
+            n_pos_deciles_range_0 = int(math.sqrt(len(deciles_to_split_0)) / 2)
+            deciles_range_0_mid_idx = len(deciles_to_split_0) // 2
+            deciles_to_split_0 = deciles_to_split_0[
+                deciles_range_0_mid_idx - n_pos_deciles_range_0
+                : deciles_range_0_mid_idx + n_pos_deciles_range_0 + 1
+            ]
+
+            n_pos_deciles_range_1 = int(math.sqrt(len(deciles_to_split_1)) / 2)
+            deciles_range_1_mid_idx = len(deciles_to_split_1) // 2
+            deciles_to_split_1 = deciles_to_split_1[
+                deciles_range_1_mid_idx - n_pos_deciles_range_1
+                : deciles_range_1_mid_idx + n_pos_deciles_range_1 + 1
+            ]
+
+        if idx_best_split_0 is None:
+            self._splits_0 = None
+        else:
+            self._splits_0 = np.quantile(
+                X[X.columns[0]],
+                [decile_to_split_0 for decile_to_split_0 in deciles_to_split_0],
+                axis=0,
+                method="nearest",
+            )  # translate the percentiles into actual values
+            assert np.isnan(self._splits_0).sum() == 0
+
+        if idx_best_split_1 is None:
+            self._splits_1 = None
+        else:
+            self._splits_1 = np.quantile(
+                X[X.columns[1]],
+                [decile_to_split_1 for decile_to_split_1 in deciles_to_split_1],
+                axis=0,
+                method="nearest",
+            )  # translate the percentiles into actual values
+            assert np.isnan(self._splits_1).sum() == 0
+
+    def predict(self, X: pd.DataFrame) -> pd.DataFrame:
+        # TODO ask: batch predict won't work here.
+        assert X.shape[1] == 2, "Exactly two features are supported"
+        assert self._positive_classes is not None, "Model not fitted"
+        assert (self._splits_0 is not None or self._splits_1 is not None), "Model not fitted"
+
+        if self._splits_0 is None:
+            output_col_0 = np.repeat(np.nan, X.shape[0])
+        else:
+            output_col_0 = \
+                np.searchsorted(self._splits_0, X[X.columns[0]].squeeze(), side="right") / len(self._splits_0)
+            if self._positive_classes[0] == 0:
+                output_col_0 = 1 - output_col_0
+
+        if self._splits_1 is None:
+            output_col_1 = np.repeat(np.nan, X.shape[0])
+        else:
+            output_col_1 = \
+                np.searchsorted(self._splits_1, X[X.columns[1]].squeeze(), side="right") / len(self._splits_1)
+            if self._positive_classes[1] == 0:
+                output_col_1 = 1 - output_col_1
+
+        return pd.DataFrame(
+            data={
+                X.columns[0]: output_col_0,
+                X.columns[1]: output_col_1,
+            },
+            index=X.index
+        )
 
 
 def calculate_bin_diff(
@@ -400,6 +494,27 @@ def calculate_bin_diff(
     agg_method: Literal["mean", "sharpe"],
 ) -> float:
     above = quantile > X
+
+    agg = np.array([np_mean(y[~above]), np_mean(y[above])])
+    if agg_method == "sharpe":
+        agg = agg / np.array([np_std(y[~above]), np_std(y[above])])
+    if len(agg) == 0:
+        return 0.0
+    if len(agg) == 1:
+        return 0.0
+    if len(agg) > 2:
+        raise AssertionError("Too many bins")
+    return np.diff(agg)[0]
+
+
+def calculate_bin_diff(
+    quantile_0: float,
+    quantile_1: float,
+    X: pd.DataFrame,
+    y: np.ndarray,
+    agg_method: Literal["mean", "sharpe"],
+) -> float:
+    above = (quantile_0 > X[X.columns[0]]) & (quantile_1 > X[X.columns[1]])
 
     agg = np.array([np_mean(y[~above]), np_mean(y[above])])
     if agg_method == "sharpe":
@@ -423,3 +538,30 @@ def np_std(x):
     if x.size == 0:
         return 0.0
     return x.std()
+
+
+def calc_deciles_to_split(best_quantile: float, num_splits: int) -> list[float]:
+    """
+    Example:
+    - If best_quantile = 0.45 and num_splits = 3, then result is [0.30, 0.35, 0.40] + [0.45] + [0.50, 0.55, 0.60]
+    - If best_quantile = 0.51 and num_splits = 2, then result is [0.41, 0.46] + [0.51] + [0.56, 0.61]
+
+    :param best_quantile: The best quantile to add splits around
+    :param num_splits: The number of splits around `best_quantile` to add in 0.05 steps
+    :return: An array sorted in ascending manner, containing `num_splits * 2 + 1` elements:
+        - `num_splits` values 0.05 apart before `best_quantile`, and
+        - `best_quantile`, and
+        - `num_splits` values 0.05 apart after `best_quantile`
+    """
+
+    range_step = 5
+
+    pos_times = 6
+    range_start = pos_times * num_splits
+
+    neg_times = -(range_start // range_step)
+    if range_start % range_step == 0:
+        neg_times += 1
+    range_stop = neg_times * range_step
+
+    return [round(best_quantile + (i * 0.01), 2) for i in range(range_stop, range_start, range_step)]
