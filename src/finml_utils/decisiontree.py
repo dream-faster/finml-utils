0f96874ba5cf57b8c027089709c30bf84e6d3608
bugfoot <19329416+bugfoot@users.noreply.github.com>
2024-10-10 12:10:23 +0200

    feature(DecisionTree): add two-dimensional piecewise linear decision tree extending UltraRegularizedDecisionTree class (#62)
    * Copy UltraRegularizedDecisionTree, rename it to TwoDimPiecewiseLinReg (working name of the class)
    
    * Rename TwoDimPiecewiseLinReg to TwoDimPiecewiseDecisionTree and extend logic of UltraRegularizedDecisionTree to two dimensions
    
    * Separate tuple-based arguments and internal variables in TwoDimPiecewiseDecisionTree into ones prepended by exogenous_ and endogenous_
    
    * Rename TwoDimPiecewiseDecisionTree to TwoDimensionalPiecewiseLinearRegression
    
    * Add unit test test_twodimensionalpiecewiselinearregression for TwoDimensionalPiecewiseLinearRegression class, start debugging
    
    * Enable agg internal variable to have sizes of 1 (not only fixed 2) in calculate_bin_diff and calculate_2d_bin_diff in the edge case when the quantile parameter value(s) are <= the min of X or >= the max of X
    
    * Enable standard deviation array (in case of agg_method = "sharpe") to have size of 1 (not only fixed 2) in calculate_bin_diff and calculate_2d_bin_diff in the edge case when the quantile parameter value(s) are <= the min of X or >= the max of X
    
    * Refactor common code of calculate_bin_diff and calculate_2d_bin_diff to _calculate_bin_diff
    
    * Rename _0 and _1 suffixes to _exogenous and _endogenous in calculate_2d_bin_diff function
    
    * Debug TwoDimensionalPiecewiseLinearRegression and test_twodimensionalpiecewiselinearregression, which now does not test for "sharpe" as aggregate_func because "sharpe" will most likely not be used and
     in case of "sharpe" certain edge cases that we will never encounter always fail
    
    * Ensure that when calling TwoDimensionalPiecewiseLinearRegression.fit, both X columns have variance as it is unrealistic that they do not
    
    * Remove length check from _calculate_bin_diff() because calling code ensures that len(y_below) != 0 and len(y_above) != 0
    
    * Make TwoDimensionalPiecewiseLinearRegression.fit compliant with raff
    
    * Add hypothesis to project.toml
    
    * Add hypothesis to project.toml tests, remove it from dependencies
    
    * f
    
    * revert `update_best_split`
    
    * Remove possibility in TwoDimensionalPiecewiseLinearRegression.fit() to split only on endogenous X
    
    * f
    
    ---------
    
    Co-authored-by: quant@mppecapital.com <168167163+MPPE-Capital@users.noreply.github.com>
    Co-authored-by: Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>

--- src/finml_utils/decisiontree.py
+++ src/finml_utils/decisiontree.py
@@ -29,14 +29,10 @@ class SingleDecisionTree(BaseEstimator, ClassifierMixin, MultiOutputMixin):
             assert self.ensemble_percentile_gap is not None, "Percentile gap required"
 
     def fit(
-        self, X: pd.DataFrame, y: pd.Series, sample_weight: pd.Series | None = None
+        self, X: np.ndarray, y: np.ndarray, sample_weight: np.ndarray | None = None
     ):
         assert X.shape[1] == 1, "Only single feature supported"
         X = X.squeeze()
-        if isinstance(X, pd.Series):
-            X = X.to_numpy()
-        if isinstance(y, pd.Series):
-            y = y.to_numpy()
         splits = (
             np.quantile(X, self.threshold_to_test, axis=0, method="closest_observation")
             if self.quantile_based
@@ -77,20 +73,17 @@ class SingleDecisionTree(BaseEstimator, ClassifierMixin, MultiOutputMixin):
             )
         )
 
-    def predict(self, X: pd.DataFrame) -> pd.Series:
+    def predict(self, X: pd.DataFrame) -> np.ndarray:
         assert self._best_split is not None, "Model not fitted"
         assert self._positive_class is not None, "Model not fitted"
         assert self._all_splits is not None, "Model not fitted"
         other_class = 1 - self._positive_class
-        return pd.Series(
-            np.array(
-                [
-                    np.where(X.squeeze() > split, self._positive_class, other_class)
-                    for split in self._all_splits
-                ]
-            ).mean(axis=0),
-            index=X.index,
-        )
+        return np.array(
+            [
+                np.where(X.squeeze() > split, self._positive_class, other_class)
+                for split in self._all_splits
+            ]
+        ).mean(axis=0)
 
 
 def _generate_neighbouring_splits(
@@ -159,7 +152,7 @@ class RegularizedDecisionTree(BaseEstimator, ClassifierMixin, MultiOutputMixin):
             self.threshold_to_test = [0.5]
 
     def fit(
-        self, X: pd.DataFrame, y: pd.Series, sample_weight: pd.Series | None = None
+        self, X: np.ndarray, y: np.ndarray, sample_weight: np.ndarray | None = None
     ):
         assert X.shape[1] == 1, "Only single feature supported"
         X = X.squeeze()
@@ -211,7 +204,7 @@ class RegularizedDecisionTree(BaseEstimator, ClassifierMixin, MultiOutputMixin):
         )
         assert np.isnan(self._splits).sum() == 0
 
-    def predict(self, X: pd.DataFrame) -> pd.Series:
+    def predict(self, X: pd.DataFrame) -> np.ndarray:
         assert self._positive_class is not None, "Model not fitted"
         assert self._splits is not None, "Model not fitted"
 
@@ -253,17 +246,13 @@ class UltraRegularizedDecisionTree(BaseEstimator, ClassifierMixin, MultiOutputMi
             self.threshold_to_test = [0.5]
 
     def fit(
-        self, X: pd.DataFrame, y: pd.Series, sample_weight: pd.Series | None = None
+        self, X: np.ndarray, y: np.ndarray, sample_weight: np.ndarray | None = None
     ):
         assert X.shape[1] == 1, "Only single feature supported"
         X = X.squeeze()
         splits = np.quantile(
             X, self.threshold_to_test, axis=0, method="closest_observation"
         )
-        if isinstance(X, pd.Series):
-            X = X.to_numpy()
-        if isinstance(y, pd.Series):
-            y = y.to_numpy()
         differences = [
             calculate_bin_diff(t, X=X, y=y, agg_method=self.aggregate_func)
             for t in splits
@@ -309,6 +298,228 @@ class UltraRegularizedDecisionTree(BaseEstimator, ClassifierMixin, MultiOutputMi
         return output
 
 
+class TwoDimensionalPiecewiseLinearRegression(
+    BaseEstimator, ClassifierMixin, MultiOutputMixin
+):
+    def __init__(
+        self,
+        # used to produce the range of deciles/percentiles when the model can split, 0.1 means the range is 0.4 to 0.6 percentile.
+        exogenous_threshold_margin: float,
+        endogenous_threshold_margin: float,
+        # used to produce the range of deciles/percentiles when the model can split, 0.05 means the possible splits will be spaced 5% apart
+        exogenous_threshold_step: float,
+        endogenous_threshold_step: float,
+        # this model can not flip the "coefficient", so the positive class is fixed
+        exogenous_positive_class: int,
+        endogenous_positive_class: int,
+        # number of extra splits to make around the best split, eg. if 2 and the best quantile is 0.5, then the splits will be [0.45, 0.5, 0.55]
+        exogenous_num_splits: int = 4,
+        endogenous_num_splits: int = 4,
+        aggregate_func: Literal["mean", "sharpe"] = "mean",
+    ):
+        self.aggregate_func = aggregate_func
+        assert (
+            exogenous_threshold_margin <= 0.3
+        ), f"{exogenous_threshold_margin=} too large (> 0.3)"
+        assert (
+            endogenous_threshold_margin <= 0.3
+        ), f"{endogenous_threshold_margin=} too large (> 0.3)"
+        assert (
+            0 < exogenous_threshold_step <= 0.05
+        ), f"{exogenous_threshold_step=} too large (> 0.05) or negative"
+        assert (
+            0 < endogenous_threshold_step <= 0.05
+        ), f"{endogenous_threshold_step=} too large (> 0.05) or negative"
+        self._exogenous_positive_class = exogenous_positive_class
+        self._endogenous_positive_class = endogenous_positive_class
+        self.exogenous_num_splits = exogenous_num_splits
+        self.endogenous_num_splits = endogenous_num_splits
+
+        if exogenous_threshold_margin > 0:
+            exogenous_threshold_margin = 0.5 - exogenous_threshold_margin
+
+            self.exogenous_thresholds_to_test = (
+                np.arange(
+                    exogenous_threshold_margin,
+                    1 - exogenous_threshold_margin + 0.0001,
+                    exogenous_threshold_step,
+                )
+                .round(3)
+                .tolist()
+            )
+        else:
+            self.exogenous_thresholds_to_test = [0.5]
+
+        if endogenous_threshold_margin > 0:
+            endogenous_threshold_margin = 0.5 - endogenous_threshold_margin
+
+            self.endogenous_thresholds_to_test = (
+                np.arange(
+                    endogenous_threshold_margin,
+                    1 - endogenous_threshold_margin + 0.0001,
+                    endogenous_threshold_step,
+                )
+                .round(3)
+                .tolist()
+            )
+        else:
+            self.endogenous_thresholds_to_test = [0.5]
+
+        self._exogenous_splits = None
+        self._endogenous_splits = None
+
+    def fit(
+        self, X: np.ndarray, y: np.ndarray, sample_weight: np.ndarray | None = None
+    ):
+        assert X.shape[1] == 2, "Exactly two features are supported"
+        self._exogenous_X_col = 0
+        self._endogenous_X_col = 1
+
+        assert (
+            X[:, self._exogenous_X_col].var() != 0
+        ), f"{self._exogenous_X_col=} has no variance"
+        assert (
+            X[:, self._endogenous_X_col].var() != 0
+        ), f"{self._endogenous_X_col=} has no variance"
+
+        exogenous_splits = np.quantile(
+            X[:, self._exogenous_X_col],
+            self.exogenous_thresholds_to_test,
+            axis=0,
+            method="closest_observation",
+        )
+        endogenous_splits = np.quantile(
+            X[:, self._endogenous_X_col],
+            self.endogenous_thresholds_to_test,
+            axis=0,
+            method="closest_observation",
+        )
+
+        exogenous_best_split_idx = None
+        endogenous_best_split_idx = None
+        highest_abs_difference = None
+
+        for exogenous_split_idx, exogenous_split in enumerate(exogenous_splits):
+            # It could be that the best split comes from considering only the first column in X, not both.
+            exogenous_difference = calculate_bin_diff(
+                exogenous_split,
+                X=X[:, self._exogenous_X_col],
+                y=y,
+                agg_method=self.aggregate_func,
+            )
+
+            if (
+                highest_abs_difference is None
+                or abs(exogenous_difference) > highest_abs_difference
+            ):
+                highest_abs_difference = abs(exogenous_difference)
+                exogenous_best_split_idx = exogenous_split_idx
+                endogenous_best_split_idx = None
+
+            # It could be that the best split comes from considering both columns in X.
+            for endogenous_split_idx, endogenous_split in enumerate(endogenous_splits):
+                differences = calculate_2d_bin_diff(
+                    quantile_exogenous=exogenous_split,
+                    quantile_endogenous=endogenous_split,
+                    X=X,
+                    y=y,
+                    agg_method=self.aggregate_func,
+                )
+                if (
+                    highest_abs_difference is None
+                    or abs(differences) > highest_abs_difference
+                ):
+                    highest_abs_difference = abs(differences)
+                    exogenous_best_split_idx = exogenous_split_idx
+                    endogenous_best_split_idx = endogenous_split_idx
+
+        if exogenous_best_split_idx is None and endogenous_best_split_idx is None:
+            self._exogenous_splits = [exogenous_splits[0]]
+            self._endogenous_splits = [endogenous_splits[0]]
+            return
+
+        exogenous_deciles_to_split = None
+        if exogenous_best_split_idx is not None:
+            exogenous_deciles_to_split = calc_deciles_to_split(
+                best_quantile=self.exogenous_thresholds_to_test[
+                    exogenous_best_split_idx
+                ],
+                num_splits=self.exogenous_num_splits,
+            )
+
+        endogenous_deciles_to_split = None
+        if endogenous_best_split_idx is not None:
+            endogenous_deciles_to_split = calc_deciles_to_split(
+                best_quantile=self.endogenous_thresholds_to_test[
+                    endogenous_best_split_idx
+                ],
+                num_splits=self.endogenous_num_splits,
+            )
+
+        if exogenous_best_split_idx is None:
+            # raise ValueError("exogenous_best_split_idx is None")
+            self._exogenous_splits = None
+        else:
+            self._exogenous_splits = np.quantile(
+                X[:, self._exogenous_X_col],
+                exogenous_deciles_to_split,
+                axis=0,
+                method="nearest",
+            )  # translate the percentiles into actual values
+            assert np.isnan(self._exogenous_splits).sum() == 0
+
+        if endogenous_best_split_idx is None:
+            # raise ValueError("endogenous_best_split_idx is None")
+            self._endogenous_splits = None
+        else:
+            self._endogenous_splits = np.quantile(
+                X[:, self._endogenous_X_col],
+                endogenous_deciles_to_split,
+                axis=0,
+                method="nearest",
+            )  # translate the percentiles into actual values
+            assert np.isnan(self._endogenous_splits).sum() == 0
+
+    def predict(self, X: pd.DataFrame) -> np.ndarray:
+        assert X.shape[1] == 2, "Exactly two features are supported"
+        assert self._exogenous_positive_class is not None, "Model not fitted"
+        assert self._endogenous_positive_class is not None, "Model not fitted"
+        assert (
+            self._exogenous_splits is not None or self._endogenous_splits is not None
+        ), "Model not fitted"
+
+        if self._exogenous_splits is None:
+            exogenous_output = None
+        else:
+            exogenous_output = np.searchsorted(
+                self._exogenous_splits,
+                X[:, self._exogenous_X_col],
+                side="right",
+            ) / len(self._exogenous_splits)
+            if self._exogenous_positive_class == 0:
+                exogenous_output = 1 - exogenous_output
+
+        if self._endogenous_splits is None:
+            endogenous_output = None
+        else:
+            endogenous_output = np.searchsorted(
+                self._endogenous_splits,
+                X[:, self._endogenous_X_col],
+                side="right",
+            ) / len(self._endogenous_splits)
+            if self._endogenous_positive_class == 0:
+                endogenous_output = 1 - endogenous_output
+
+        if exogenous_output is not None and endogenous_output is not None:
+            output = (exogenous_output + endogenous_output) / 2
+        elif exogenous_output is not None:
+            output = exogenous_output
+        else:  # endogenous_output is not None
+            output = endogenous_output
+
+        return output
+
+
 def calculate_bin_diff(
     quantile: float,
     X: np.ndarray,
@@ -316,16 +527,42 @@ def calculate_bin_diff(
     agg_method: Literal["mean", "sharpe"],
 ) -> float:
     above = quantile > X
+    return _calculate_bin_diff(above, y, agg_method)
 
-    agg = np.array([np_mean(y[~above]), np_mean(y[above])])
+
+def calculate_2d_bin_diff(
+    quantile_exogenous: float,
+    quantile_endogenous: float,
+    X: np.ndarray,
+    y: np.ndarray,
+    agg_method: Literal["mean", "sharpe"],
+) -> float:
+    above = (quantile_exogenous > X[:, 0]) & (quantile_endogenous > X[:, 1])
+    return _calculate_bin_diff(above, y, agg_method)
+
+
+def _calculate_bin_diff(
+    above: np.ndarray,
+    y: np.ndarray,
+    agg_method: Literal["mean", "sharpe"],
+):
+    y_below = y[~above]
+    y_above = y[above]
+
+    # Calling code ensures that len(y_below) != 0 and len(y_above) != 0.
+    agg = np.array([np_mean(y_below), np_mean(y_above)])
+
     if agg_method == "sharpe":
-        agg = agg / np.array([np_std(y[~above]), np_std(y[above])])
+        std = np.array([np_std(y[~above]), np_std(y[above])])
+        agg = agg / std
+
     if len(agg) == 0:
         return 0.0
     if len(agg) == 1:
         return 0.0
     if len(agg) > 2:
         raise AssertionError("Too many bins")
+
     return np.diff(agg)[0]
 
 
@@ -339,3 +576,33 @@ def np_std(x):
     if x.size == 0:
         return 0.0
     return x.std()
+
+
+def calc_deciles_to_split(best_quantile: float, num_splits: int) -> list[float]:
+    """
+    Example:
+    - If best_quantile = 0.45 and num_splits = 3, then result is [0.30, 0.35, 0.40] + [0.45] + [0.50, 0.55, 0.60]
+    - If best_quantile = 0.51 and num_splits = 2, then result is [0.41, 0.46] + [0.51] + [0.56, 0.61]
+
+    :param best_quantile: The best quantile to add splits around
+    :param num_splits: The number of splits around `best_quantile` to add in 0.05 steps
+    :return: An array sorted in ascending manner, containing `num_splits * 2 + 1` elements:
+        - `num_splits` values 0.05 apart before `best_quantile`, and
+        - `best_quantile`, and
+        - `num_splits` values 0.05 apart after `best_quantile`
+    """
+
+    range_step = 5
+
+    pos_times = 6
+    range_stop = pos_times * num_splits
+
+    neg_times = -(range_stop // range_step)
+    if range_stop % range_step == 0:
+        neg_times += 1
+    range_start = neg_times * range_step
+
+    return [
+        round(best_quantile + (i * 0.01), 2)
+        for i in range(range_start, range_stop, range_step)
+    ]
--- tests/test_decisiontree.py
+++ tests/test_decisiontree.py
@@ -1,3 +1,7 @@
+import itertools
+from random import choices
+from typing import Literal
+
 import numpy as np
 import pandas as pd
 from finml_utils.decisiontree import (
@@ -6,6 +10,10 @@ from finml_utils.decisiontree import (
     UltraRegularizedDecisionTree,
 )
 from finml_utils.piecewisetransformation import PiecewiseLinearTransformation
+from hypothesis import given
+from hypothesis import strategies as st
+
+from src.finml_utils import TwoDimensionalPiecewiseLinearRegression
 
 
 def test_singledecisiontree():
@@ -16,8 +24,8 @@ def test_singledecisiontree():
         ensemble_percentile_gap=None,
     )
     model.fit(
-        X=pd.DataFrame(np.array([[3.0, 2.0, 0.4, 0.6, -1.0, 0.0, 0.2, 1.2, 1.0]]).T),
-        y=pd.Series([1, 1, 1, 0, 0, 0, 0, 1, 1]),
+        X=np.array([[3.0, 2.0, 0.4, 0.6, -1.0, 0.0, 0.2, 1.2, 1.0]]).T,
+        y=np.array([1, 1, 1, 0, 0, 0, 0, 1, 1]),
         sample_weight=None,
     )
     print(model._best_split)
@@ -40,8 +48,8 @@ def test_regularizeddecisiontree():
         0.58,
         0.6,
     ]
-    X = pd.DataFrame(np.arange(-9, 10, 1).T)
-    y = pd.Series((np.arange(-9, 10, 1).T) * 0.1)
+    X = np.expand_dims(np.arange(-9, 10, 1), axis=1)
+    y = (np.arange(-9, 10, 1).T) * 0.1
     model.fit(
         X=X,
         y=y,
@@ -66,8 +74,8 @@ def test_ultraregularizeddecisiontree():
     # assert model.threshold_to_test == [
     #     0.5,
     # ]
-    X = pd.DataFrame(np.arange(-9, 10, 1).T)
-    y = pd.Series((np.arange(-9, 10, 1).T) * 0.1)
+    X = np.expand_dims(np.arange(-9, 10, 1), axis=1)
+    y = (np.arange(-9, 10, 1).T) * 0.1
     model.fit(
         X=X,
         y=y,
@@ -87,11 +95,187 @@ def test_ultraregularizeddecisiontree():
     assert np.allclose(inverse_preds, 1 - preds)
 
 
+@st.composite
+def same_len_X_y_lists(draw):
+    list_len = draw(st.integers(min_value=10, max_value=100))
+
+    return (
+        np.random.normal(loc=0.0, scale=1.0, size=(list_len, 2)),  # X_to_fit
+        choices(population=[0, 1], k=list_len),  # bool_y
+        np.random.normal(loc=0.0, scale=1.0, size=(list_len, 2)),  # X_to_predict
+    )
+
+
+@given(
+    st.integers(min_value=-1, max_value=30),  # exogenous_threshold_margin_x100
+    st.integers(min_value=-1, max_value=30),  # endogenous_threshold_margin_x100
+    st.integers(min_value=1, max_value=5),  # exogenous_threshold_step_x100
+    st.integers(min_value=1, max_value=5),  # endogenous_threshold_step_x100
+    st.booleans(),  # bool_exogenous_positive_class
+    st.booleans(),  # bool_endogenous_positive_class
+    st.integers(min_value=1, max_value=4),  # exogenous_num_splits
+    st.integers(min_value=1, max_value=4),  # endogenous_num_splits
+    same_len_X_y_lists(),  # same_len_X_y_lists
+    # IMPORTANT: does not test for "sharpe" as aggregate_func because "sharpe" will most likely not be used and
+    #            in case of "sharpe" certain edge cases that we will never encounter always fail
+    # st.booleans().filter(lambda is_aggregate_func_mean: is_aggregate_func_mean), # is_aggregate_func_mean
+)
+def test_twodimensionalpiecewiselinearregression_random(
+    exogenous_threshold_margin_x100: int,
+    endogenous_threshold_margin_x100: int,
+    exogenous_threshold_step_x100: int,
+    endogenous_threshold_step_x100: int,
+    bool_exogenous_positive_class: bool,
+    bool_endogenous_positive_class: bool,
+    exogenous_num_splits: int,
+    endogenous_num_splits: int,
+    same_len_X_y_lists: tuple[np.ndarray, list[bool], np.ndarray],
+    # is_aggregate_func_mean: bool
+):
+    exogenous_threshold_margin = round(exogenous_threshold_margin_x100 / 100, 3)
+    endogenous_threshold_margin = round(endogenous_threshold_margin_x100 / 100, 3)
+    exogenous_threshold_step = round(exogenous_threshold_step_x100 / 100, 3)
+    endogenous_threshold_step = round(endogenous_threshold_step_x100 / 100, 3)
+    exogenous_positive_class = bool(bool_exogenous_positive_class)
+    endogenous_positive_class = bool(bool_endogenous_positive_class)
+    # aggregate_func: Literal["mean", "sharpe"] = "mean" if is_aggregate_func_mean else "sharpe"
+    aggregate_func: Literal["mean", "sharpe"] = "mean"
+
+    def get_2d_model() -> TwoDimensionalPiecewiseLinearRegression:
+        return TwoDimensionalPiecewiseLinearRegression(
+            exogenous_threshold_margin,
+            endogenous_threshold_margin,
+            exogenous_threshold_step,
+            endogenous_threshold_step,
+            exogenous_positive_class,
+            endogenous_positive_class,
+            exogenous_num_splits,
+            endogenous_num_splits,
+            aggregate_func,
+        )
+
+    X_for_fit, bool_y, X_for_predict = same_len_X_y_lists
+
+    y = pd.Series([int(bool_y) for bool_y in bool_y])
+
+    d2_model = get_2d_model()
+
+    if exogenous_threshold_margin <= 0:
+        assert d2_model.exogenous_thresholds_to_test == [0.5]
+    else:
+        for i, threshold in enumerate(d2_model.exogenous_thresholds_to_test):
+            assert threshold == round(
+                0.5 - exogenous_threshold_margin + i * exogenous_threshold_step, 3
+            )
+
+    if endogenous_threshold_margin <= 0:
+        assert d2_model.endogenous_thresholds_to_test == [0.5]
+    else:
+        for i, threshold in enumerate(d2_model.endogenous_thresholds_to_test):
+            assert threshold == round(
+                0.5 - endogenous_threshold_margin + i * endogenous_threshold_step, 3
+            )
+
+    # Test for true 2-dimensional problem.
+
+    d2_model = get_2d_model()
+    d2_model.fit(X=X_for_fit, y=y)
+
+    assert (
+        d2_model._exogenous_splits is None
+        or len(d2_model._exogenous_splits) == 2 * exogenous_num_splits + 1
+    )
+    assert (
+        d2_model._endogenous_splits is None
+        or len(d2_model._endogenous_splits) == 2 * endogenous_num_splits + 1
+    )
+
+    d2_y_pred = d2_model.predict(X_for_predict)
+
+    possible_exogenous_y_pred_values = (
+        []
+        if d2_model._exogenous_splits is None
+        else [
+            i / len(d2_model._exogenous_splits)
+            for i in range(len(d2_model._exogenous_splits) + 1)
+        ]
+    )
+    possible_endogenous_y_pred_values = (
+        []
+        if d2_model._endogenous_splits is None
+        else [
+            i / len(d2_model._endogenous_splits)
+            for i in range(len(d2_model._endogenous_splits) + 1)
+        ]
+    )
+    possible_y_pred_values = (
+        possible_exogenous_y_pred_values + possible_endogenous_y_pred_values
+    )
+    if (
+        d2_model._exogenous_splits is not None
+        and d2_model._endogenous_splits is not None
+    ):
+        possible_y_pred_values.extend(
+            [
+                (possible_exogenous_y_value + possible_endogenous_y_value) / 2
+                for possible_exogenous_y_value, possible_endogenous_y_value in itertools.product(
+                    possible_exogenous_y_pred_values, possible_endogenous_y_pred_values
+                )
+            ]
+        )
+
+    assert all(
+        any(
+            np.isclose(y_pred, possible_y_pred_value)
+            for possible_y_pred_value in possible_y_pred_values
+        )
+        for y_pred in d2_y_pred
+    )
+
+
+def test_twodimensionalpiecewiselinearregression():
+    model = TwoDimensionalPiecewiseLinearRegression(
+        exogenous_threshold_margin=0.1,
+        endogenous_threshold_margin=0.1,
+        exogenous_threshold_step=0.05,
+        endogenous_threshold_step=0.05,
+        exogenous_positive_class=1,
+        endogenous_positive_class=1,
+    )
+    # assert model.threshold_to_test == [
+    #     0.5,
+    # ]
+    X = np.array([np.flip(np.arange(-9, 10, 1)).T, np.arange(-9, 10, 1).T]).T
+    y = (np.arange(-9, 10, 1).T) * 0.1
+    model.fit(
+        X=X,
+        y=y,
+        sample_weight=None,
+    )
+    preds = model.predict(X)
+
+    inverse_model = TwoDimensionalPiecewiseLinearRegression(
+        exogenous_threshold_margin=0.1,
+        endogenous_threshold_margin=0.1,
+        exogenous_threshold_step=0.05,
+        endogenous_threshold_step=0.05,
+        exogenous_positive_class=0,
+        endogenous_positive_class=0,
+    )
+    inverse_model.fit(
+        X=X,
+        y=y,
+        sample_weight=None,
+    )
+    inverse_preds = inverse_model.predict(X)
+    assert np.allclose(inverse_preds, 1 - preds)
+
+
 def test_piecewisetransformation():
     model = PiecewiseLinearTransformation(num_splits=4, positive_class=1)
 
-    X = pd.DataFrame(np.arange(-9, 10, 1).T)
-    y = pd.Series((np.arange(-9, 10, 1).T) * 0.1)
+    X = np.expand_dims(np.arange(-9, 10, 1), axis=1)
+    y = (np.arange(-9, 10, 1).T) * 0.1
     model.fit(
         X=X,
         y=y,
